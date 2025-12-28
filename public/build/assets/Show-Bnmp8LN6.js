454b92ae82e59e0ab21523ddf5f2da4af7e3790f
Astha <iyandabes1@gmail.com>
2025-12-29 00:29:03 +0700

    build

---
+++ public/build/assets/Show-Bnmp8LN6.js
@@ -0,0 +1,1 @@
+import{j as i}from"./app-4pfQmxH3.js";import"./app-CzM0zAVH.js";function t(){return i.jsx("div",{children:"KlinikShow"})}export{t as default};
--- public/build/assets/Show-D8mCprw4.js
+++
@@ -1,1 +0,0 @@
-import{j as i}from"./app-Cj27gzC2.js";import"./app-WK0bYjlf.js";function t(){return i.jsx("div",{children:"KlinikShow"})}export{t as default};
